d33f010b4ac81a475dca9bed7ec83a8736054dde
lundjrl <jrlbidamin@gmail.com>
2018-12-08 19:19:25 -0500

    Almost done with portion 1

--- Bash1/backup.sh
+++ Bash1/backup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-target=$backupDir
+dest=$backupDir
 target2=$sourceDir
 
 file=$1
 file2=$2
-while
-do
-if [ -e $1 ] then
-	echo "Sweet, the file exists in source""
-	else 
-		"File does not exist, copying"
-		cp $1 $backupDir
+
+DIFF=$(diff "$1" "$2")
+
+if [ -e "$1" ] 
+then
+	echo "Sweet, the file exists in source"
+else 
+	"File does not exist, copying"
+	cp -v $1 $backupDir
 fi
 
-done
 
-exitp
+if [ "$DIFF" != "" ]
+then 
+	echo "File is not recent copy, copying to dir"
+	cp -v $1 $backupDir
+fi
+
+exit
