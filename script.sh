c408a597ff225d7b6f740977b74fb935b54b3ca4
lundjrl <jrlbidamin@gmail.com>
2018-12-09 15:42:15 -0500

    Started portion 2 of project 3, much is still needed

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+file="cis241.zip"
+unzip -d file
+
+ls data > temp
+while read line
+do 
+    IFS='_'
+    file=$line
+    unzip -d file
+    username=$2
+    filename=$5
+    restore IFS
+mv $5 $2
+done < temp    
