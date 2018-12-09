58a7c70d55bd807376eff7853c246e2c3bdcdf38
lundjrl <jrlbidamin@gmail.com>
2018-12-08 20:18:45 -0500

    Worked on more code...

--- Bash1/backup.sh
+++ Bash1/backup.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
 dest=$backupDir
-target2=$sourceDir
+target=$sourceDir
 
 file=$1
 file2=$2
 
 DIFF=$(diff "$1" "$2")
 
-if [ -e "$1" ] 
-then
-	echo "Sweet, the file exists in source"
-else 
-	"File does not exist, copying"
-	cp -v $1 $backupDir
-fi
+#Will not copy over contents of files
+if [ ! -d "$dest" ]; then
+    #Creates a new backup directory if one does not exist
+    echo "Creating backup directory"
+    mkdir backupDir
 
+    if [ -e "$1" ]; 
+    then
+	#Checking if file exists 
+	echo "Sweet, the file exists in source directory"
+    else 
+	#If the file does not exist
+	"File does not exist, please select new file"
+	cp -v $1 $dest
+    fi
 
-if [ "$DIFF" != "" ]
-then 
+    if [ "$DIFF" != "" ];
+    then
+	#Comparing two files if they are not the same.  
 	echo "File is not recent copy, copying to dir"
-	cp -v $1 $backupDir
+	cp -v $1 $dest
+    else
+	echo "File is up to date :)"
+    fi
 fi
 
 exit
